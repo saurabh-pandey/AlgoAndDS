bb8ce42895ab62b170e001141fb0939098e39e3f
Saurabh Pandey <saurabhpandey85@gmail.com>
2017-04-14 13:37:39 +0530

    Maximum Subarray Problem solution using DP

---
+++ maxSubArrayProblem.py
@@ -0,0 +1,19 @@
+import sys
+
+def maxSubArray(arr):
+    maxSubArr = []
+    maxSum = -sys.maxint -1
+    sumSoFar = 0
+    subArrSoFar = []
+    for i in arr:
+        sumSoFar += i
+        subArrSoFar.append(i)
+        if (maxSum < sumSoFar):
+            maxSum = sumSoFar
+            maxSubArr = subArrSoFar[:]
+        if (sumSoFar <= 0):
+            sumSoFar = 0
+            del subArrSoFar[:]
+    return (maxSum, maxSubArr)
+
+
