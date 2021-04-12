1320c4da481494557719c09ac75522fc27318904
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-04-12 09:39:59 +0530

    LC-LinkedList: Done with finding cycle begin node. But is On^2

---
+++ leetcode/linkedList/cycle_begin_node_SLL.py
@@ -0,0 +1,63 @@
+#URL: https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1214/
+# Description
+"""
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+There is a cycle in a linked list if there is some node in the list that can be reached again by 
+continuously following the next pointer. Internally, pos is used to denote the index of the node 
+that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Notice that you should not modify the linked list.
+
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ 
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+"""
+from SinglyLinkedList import SinglyLinkedList
+
+def cycleBeginNode(head):
+  distance = 0
+  traceNode = head
+  depth = 0
+  currNode = head
+  while currNode is not None:
+    depth += 1
+    currNode = currNode._next
+    while traceNode is not None:
+      if traceNode is currNode:
+        break
+      traceNode = traceNode._next
+      distance += 1
+    # if currNode is not None:
+      # print(f'Distance from head to {currNode.val} is {distance} and depth = {depth}')
+    if distance != depth:
+      # print(f'WARNING: Cycle detected at distance = {distance} and depth = {depth}')
+      # print(f'WARNING: Cycle starting at node = {traceNode.val}')
+      return traceNode
+    traceNode = head
+    distance = 0
+  return None
--- leetcode/linkedList/test_cycle_begin_node_SLL.py
+++ leetcode/linkedList/test_cycle_begin_node_SLL.py
@@ -1,9 +1,7 @@
-import pytest
-
 from SinglyLinkedList import SinglyLinkedList
 
 import create_cycle_SLL as sll_util
-import detect_cycle_SLL as prob
+import cycle_begin_node_SLL as prob
 
 class TestDetectCycleSLL:
   def test_detect_cycle1(self):
@@ -11,24 +9,22 @@ class TestDetectCycleSLL:
 
     head = myList.getHeadNode()
 
-    assert prob.detectCycleSLL(head) == False
+    assert prob.cycleBeginNode(head) == None
     
     sll_util.createCycleSLL(myList,1)
 
-    assert prob.detectCycleSLL(head) == True
-  
+    assert prob.cycleBeginNode(head)._val == 2
   
   def test_detect_cycle2(self):
     myList = SinglyLinkedList([1,2,3,4])
 
     head = myList.getHeadNode()
 
-    assert prob.detectCycleSLL(head) == False
+    assert prob.cycleBeginNode(head) == None
     
     sll_util.createCycleSLL(myList,0)
 
-    assert prob.detectCycleSLL(head) == True
-  
+    assert prob.cycleBeginNode(head)._val == 1
   
   def test_detect_cycle3(self):
     myList = SinglyLinkedList([-1,-7,7,-4,19,6,-9,-5,-2,-5])
@@ -37,4 +33,4 @@ class TestDetectCycleSLL:
 
     head = myList.getHeadNode()
 
-    assert prob.detectCycleSLL(head) == True
+    assert prob.cycleBeginNode(head)._val == -9
