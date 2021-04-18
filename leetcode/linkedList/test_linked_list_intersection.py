325b32523c73798963913fe4a90e0c38114dfd08
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-04-18 17:54:41 +0530

    LC-LinkedList: Creating intersected lists in test code

--- leetcode/linkedList/test_linked_list_intersection.py
+++ leetcode/linkedList/test_linked_list_intersection.py
@@ -14,3 +14,19 @@ class TestLinkedListIntersection:
     # Use above input to create intersection
     headA = SinglyLinkedList.Node(4)
     headB = SinglyLinkedList.Node(5)
+    currNodeA = headA
+    for i in range(1, skipA):
+      currNodeA._next = SinglyLinkedList.Node(listA[i])
+      currNodeA = currNodeA._next
+    
+    currNodeB = headB
+    for i in range(1, skipB):
+      currNodeB._next = SinglyLinkedList.Node(listB[i])
+      currNodeB = currNodeB._next
+    
+    for i in range(skipA, len(listA)):
+      intersectedNode = SinglyLinkedList.Node(listA[i])
+      currNodeA._next = intersectedNode
+      currNodeB._next = intersectedNode
+      currNodeA = currNodeA._next
+      currNodeB = currNodeB._next
