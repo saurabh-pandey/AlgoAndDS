a853e9560cbd57e8d9e4fc91a23794f22fcc391b
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-04-20 11:55:44 +0530

    LC-LinkedList: Done with all intersection tests

--- leetcode/linkedList/test_linked_list_intersection.py
+++ leetcode/linkedList/test_linked_list_intersection.py
@@ -12,8 +12,8 @@ class TestLinkedListIntersection:
     skipA = 2
     skipB = 3
     # Use above input to create intersection
-    headA = SinglyLinkedList.Node(4)
-    headB = SinglyLinkedList.Node(5)
+    headA = SinglyLinkedList.Node(listA[0])
+    headB = SinglyLinkedList.Node(listB[0])
     currNodeA = headA
     for i in range(1, skipA):
       currNodeA._next = SinglyLinkedList.Node(listA[i])
@@ -32,3 +32,64 @@ class TestLinkedListIntersection:
       currNodeB = currNodeB._next
     
     assert prob.getIntersectionNode(headA, headB)._val == intersectVal
+  
+  
+  def test_example2(self):
+    intersectVal = 2
+    listA = [1,9,1,2,4]
+    listB = [3,2,4]
+    skipA = 3
+    skipB = 1
+    
+    # Use above input to create intersection
+    headA = SinglyLinkedList.Node(listA[0])
+    headB = SinglyLinkedList.Node(listB[0])
+    currNodeA = headA
+    for i in range(1, skipA):
+      currNodeA._next = SinglyLinkedList.Node(listA[i])
+      currNodeA = currNodeA._next
+    
+    currNodeB = headB
+    for i in range(1, skipB):
+      currNodeB._next = SinglyLinkedList.Node(listB[i])
+      currNodeB = currNodeB._next
+    
+    for i in range(skipA, len(listA)):
+      intersectedNode = SinglyLinkedList.Node(listA[i])
+      currNodeA._next = intersectedNode
+      currNodeB._next = intersectedNode
+      currNodeA = currNodeA._next
+      currNodeB = currNodeB._next
+    
+    assert prob.getIntersectionNode(headA, headB)._val == intersectVal
+  
+
+  def test_example3(self):
+    intersectVal = 0
+    listA = [2,6,4]
+    listB = [1,5]
+    skipA = 3
+    skipB = 2
+    
+    # Use above input to create intersection
+    headA = SinglyLinkedList.Node(listA[0])
+    headB = SinglyLinkedList.Node(listB[0])
+    currNodeA = headA
+    for i in range(1, skipA):
+      currNodeA._next = SinglyLinkedList.Node(listA[i])
+      currNodeA = currNodeA._next
+    
+    currNodeB = headB
+    for i in range(1, skipB):
+      currNodeB._next = SinglyLinkedList.Node(listB[i])
+      currNodeB = currNodeB._next
+    
+    for i in range(skipA, len(listA)):
+      intersectedNode = SinglyLinkedList.Node(listA[i])
+      currNodeA._next = intersectedNode
+      currNodeB._next = intersectedNode
+      currNodeA = currNodeA._next
+      currNodeB = currNodeB._next
+    
+    assert prob.getIntersectionNode(headA, headB) is None
+
