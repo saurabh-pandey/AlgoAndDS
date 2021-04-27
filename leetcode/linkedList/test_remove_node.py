982e1b7a1bdc10c68cb0d7371706a97bcbd5cc06
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-04-27 12:45:11 +0530

    LC-LinkedList: Added some tests for removing node

--- leetcode/linkedList/test_remove_node.py
+++ leetcode/linkedList/test_remove_node.py
@@ -13,10 +13,42 @@ class TestRemoveNode:
       currNode = currNode._next
     return output
   
-  
+
   def test_example1(self):
+    origList = [1,2,6,3,4,5,6]
+    val = 6
+    myList = SinglyLinkedList(origList)
+    newList = prob.removeElements(myList.getHeadNode(), val)
+    assert self._toList(newList) == [1,2,3,4,5]
+  
+
+  def test_example2(self):
+    origList = []
+    val = 1
+    myList = SinglyLinkedList(origList)
+    newList = prob.removeElements(myList.getHeadNode(), val)
+    assert self._toList(newList) == []
+  
+
+  def test_example3(self):
+    origList = [7,7,7,7]
+    val = 7
+    myList = SinglyLinkedList(origList)
+    newList = prob.removeElements(myList.getHeadNode(), val)
+    assert self._toList(newList) == []
+  
+
+  def test_my_example(self):
+    origList = [1,2,3,4]
+    val = 5
+    myList = SinglyLinkedList(origList)
+    newList = prob.removeElements(myList.getHeadNode(), val)
+    assert self._toList(newList) == [1,2,3,4]
+
+  
+  def test_lc_example1(self):
     origList = [1,2,2,1]
     val = 2
     myList = SinglyLinkedList(origList)
     newList = prob.removeElements(myList.getHeadNode(), val)
-    print(f'\t### origList = {origList}, newList = {self._toList(newList)}')
+    assert self._toList(newList) == [1,1]
