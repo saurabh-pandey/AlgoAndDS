ba1816849f457864159b3a156f289281a1d7c745
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-05-16 15:15:38 +0530

    LC-LinkedList: Done deep-copy of list

--- leetcode/linkedList/problems/tests/test_list_deep_copy.py
+++ leetcode/linkedList/problems/tests/test_list_deep_copy.py
@@ -47,8 +47,29 @@ class TestListDeepCopy:
     input = [[7,None],[13,0],[11,4],[10,2],[1,0]]
 
     head = createRandomList(input)
-    # print(f'Random List = {toList(head)}')
     copiedList = prob.copyRandomList(head)
+    assert toList(copiedList) == toList(head)
+  
 
-    print(f'Copied Random List = {toList(copiedList)}')
-    # assert toList(copiedList) == input
+  def test_example2(self):
+    input = [[1,1],[2,1]]
+
+    head = createRandomList(input)
+    copiedList = prob.copyRandomList(head)
+    assert toList(copiedList) == toList(head)
+  
+
+  def test_example3(self):
+    input = [[3,None],[3,0],[3,None]]
+
+    head = createRandomList(input)
+    copiedList = prob.copyRandomList(head)
+    assert toList(copiedList) == toList(head)
+  
+
+  def test_example4(self):
+    input = []
+
+    head = createRandomList(input)
+    copiedList = prob.copyRandomList(head)
+    assert toList(copiedList) == toList(head)
