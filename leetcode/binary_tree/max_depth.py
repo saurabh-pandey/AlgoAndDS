f8906c6cafc34fdf93c234d865ee9c3ce136f753
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-06-10 11:24:57 +0530

    LC-BinaryTree: Done finding max-depth using top-down approach

---
+++ leetcode/binary_tree/max_depth.py
@@ -0,0 +1,68 @@
+#URL: https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/535/
+#Description
+"""
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down 
+to the farthest leaf node.
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+
+
+Example 3:
+
+Input: root = []
+Output: 0
+
+
+Example 4:
+
+Input: root = [0]
+Output: 1
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+"""
+max_depth = 0
+
+def maxDepthTopDownImpl(node, depth):
+  if node.left is None and node.right is None:
+    global max_depth
+    max_depth = depth if depth > max_depth else max_depth
+  if node.left is not None:
+    maxDepthTopDownImpl(node.left, depth + 1)
+  if node.right is not None:
+    maxDepthTopDownImpl(node.right, depth + 1)
+
+
+def maxDepthTopDown(root):
+  if root is None:
+    return 0
+  
+  global max_depth
+  max_depth = 0
+  maxDepthTopDownImpl(root, 1)
+  return max_depth
+
+
+def maxDepthBottomUp(root):
+  pass
+
+
+def maxDepth(root, doTopDown=True):
+  if doTopDown:
+    return maxDepthTopDown(root)
+  else:
+    return maxDepthBottomUp(root)
---
+++ leetcode/binary_tree/tests/test_max_depth.py
@@ -0,0 +1,32 @@
+import pytest
+
+import binary_tree.max_depth as prob
+import binary_tree.operations as bTree
+
+class TestMaxDepth:
+  def test_example1(self):
+    root = [3,9,20,None,None,15,7]
+    res = 3
+    rootNode = bTree.create(root)
+    assert prob.maxDepth(rootNode) == res
+  
+
+  def test_example2(self):
+    root = [1,None,2]
+    res = 2
+    rootNode = bTree.create(root)
+    assert prob.maxDepth(rootNode) == res
+  
+
+  def test_example3(self):
+    root = []
+    res = 0
+    rootNode = bTree.create(root)
+    assert prob.maxDepth(rootNode) == res
+  
+
+  def test_example4(self):
+    root = [0]
+    res = 1
+    rootNode = bTree.create(root)
+    assert prob.maxDepth(rootNode) == res
