e9f63c5a9152e6ab9489f5eec6ef12b4ff816e2c
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-06-18 13:25:53 +0530

    LC-BinaryTree: Added test for next right pointer in a tree

--- leetcode/binary_tree/tests/test_next_right_pointer.py
+++ leetcode/binary_tree/tests/test_next_right_pointer.py
@@ -1,17 +1,53 @@
 import pytest
 
 import binary_tree.next_right_pointer as prob
-import binary_tree.operations as bTree
+
+def create(in_list):
+  if len(in_list) == 0:
+    return None
+  if in_list[0] == None:
+    return None
+  
+  val_queue = in_list[1:]
+  root = prob.Node(in_list[0])
+  nodes_queue = [root]
+  while len(val_queue) > 0:
+    leftVal = None if len(val_queue) == 0 else val_queue.pop(0)
+    rightVal = None if len(val_queue) == 0 else val_queue.pop(0)
+
+    currNode = nodes_queue.pop(0)
+    if leftVal is not None:
+      leftNode = prob.Node(leftVal)
+      currNode.left = leftNode
+      nodes_queue.append(leftNode)
+    if rightVal is not None:
+      rightNode = prob.Node(rightVal)
+      currNode.right = rightNode
+      nodes_queue.append(rightNode)
+  return root
+
 
 def getNextRightList(root):
+  if root is None:
+    return []
+  
   nextRightList = []
-  node = root
-  while node is not None:
+  nodes_queue = [root]
+  while len(nodes_queue) > 0:
+    node = nodes_queue.pop(0)
     nextRightList.append(node.val)
+    if node.next is None:
+      nextRightList.append(None)
+    if node.left is not None:
+      nodes_queue.append(node.left)
+    if node.right is not None:
+      nodes_queue.append(node.right)
+  return nextRightList
 
 class TestNextRightPointer:
   def test_example1(self):
     root = [1,2,3,4,5,6,7]
-    rootNode = bTree.create(root)
+    rootNode = create(root)
     res = [1,None,2,3,None,4,5,6,7,None]
-    newRootNode = prob.connect(root)
+    newRootNode = prob.connect(rootNode)
+    assert res == getNextRightList(newRootNode)
