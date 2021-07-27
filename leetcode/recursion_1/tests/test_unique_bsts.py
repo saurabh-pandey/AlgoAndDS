105b81d192d4ac6db46420d2d252d268d66aeb16
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-07-27 12:46:36 +0530

    LC-Recursion-1: Done with generating unique BSTs

--- leetcode/recursion_1/tests/test_unique_bsts.py
+++ leetcode/recursion_1/tests/test_unique_bsts.py
@@ -2,9 +2,65 @@ import pytest
 
 import recursion_1.unique_bsts as prob
 
+def toList(root):
+  if root is None:
+    return []
+  
+  treeList = [root.val]
+  nodes_queue = [root]
+  while len(nodes_queue) > 0:
+    node = nodes_queue.pop(0)
+    lChild = node.left
+    rChild = node.right
+    if lChild is None and rChild is None:
+      continue
+    if lChild is not None:
+      treeList.append(lChild.val)
+      nodes_queue.append(lChild)
+    else:
+      treeList.append(None)
+    if rChild is not None:
+      treeList.append(rChild.val)
+      nodes_queue.append(rChild)
+    else:
+      treeList.append(None)
+  # Remove trailing None
+  while treeList[-1] == None:
+    treeList.pop()
+  return treeList
+
 class TestUniqueBsts:
   def test_example1(self):
     n = 3
     res = [[1,None,2,None,3],[1,None,3,2],[2,1,3],[3,1,None,None,2],[3,2,None,1]]
-    prob.generateTrees(3)
-    # assert prob.generateTrees(n) == res
+    bsts = prob.generateTrees(n)
+    resBsts = []
+    for bst in bsts:
+      resBsts.append(toList(bst))
+    res.sort()
+    resBsts.sort()
+    assert res == resBsts
+  
+
+  def test_example2(self):
+    n = 1
+    res = [[1]]
+    bsts = prob.generateTrees(n)
+    resBsts = []
+    for bst in bsts:
+      resBsts.append(toList(bst))
+    res.sort()
+    resBsts.sort()
+    assert res == resBsts
+  
+
+  def test_my_example1(self):
+    n = 2
+    res = [[2,1], [1,None,2]]
+    bsts = prob.generateTrees(n)
+    resBsts = []
+    for bst in bsts:
+      resBsts.append(toList(bst))
+    res.sort()
+    resBsts.sort()
+    assert res == resBsts
--- leetcode/recursion_1/unique_bsts.py
+++ leetcode/recursion_1/unique_bsts.py
@@ -21,7 +21,6 @@ Constraints:
 
 1 <= n <= 8
 """
-import pdb
 
 class TreeNode:
   def __init__(self, val, left=None, right=None):
@@ -42,29 +41,63 @@ def inorder_traversal(node):
   inorder_traversal(node.right)
 
 
-def toList(root):
+def cloneTree(root):
   if root is None:
-    return []
-  
-  treeList = [root.val]
-  nodes_queue = [root]
-  while len(nodes_queue) > 0:
-    node = nodes_queue.pop(0)
-    lChild = node.left
-    rChild = node.right
-    if lChild is None and rChild is None:
-      continue
-    if lChild is not None:
-      treeList.append(lChild.val)
-      nodes_queue.append(lChild)
+    return None
+  newRoot = TreeNode(root.val)
+  nodes = [root]
+  newNodes = [newRoot]
+  while len(nodes) > 0:
+    node = nodes.pop(0)
+    newNode = newNodes.pop(0)
+    if node.left is not None:
+      newNode.left = TreeNode(node.left.val)
+      nodes.append(node.left)
+      newNodes.append(newNode.left)
+    if node.right is not None:
+      newNode.right = TreeNode(node.right.val)
+      nodes.append(node.right)
+      newNodes.append(newNode.right)
+  return newRoot
+
+
+def gatherAllOneSidedTrees(n, treesFound, treesMap):
+  for root in treesMap[n-1]:
+    newRoot1 = TreeNode(0)
+    newRoot1.left = cloneTree(root)
+    treesFound.append(newRoot1)
+    newRoot2 = TreeNode(0)
+    newRoot2.right = cloneTree(root)
+    treesFound.append(newRoot2)
+
+
+def gatherAllBothSidedTrees(n, treesFound, treesMap):
+  i = 1
+  j = n - 2
+  while i > 0 and j > 0 and i <= j:
+    if i != j:
+      subTrees1 = treesMap[i]
+      subTrees2 = treesMap[j]
+      for root1 in subTrees1:
+        for root2 in subTrees2:
+          newRoot1 = TreeNode(0)
+          newRoot1.left = cloneTree(root1)
+          newRoot1.right = cloneTree(root2)
+          treesFound.append(newRoot1)
+          newRoot2 = TreeNode(0)
+          newRoot2.left = cloneTree(root2)
+          newRoot2.right = cloneTree(root1)
+          treesFound.append(newRoot2)
     else:
-      treeList.append(None)
-    if rChild is not None:
-      treeList.append(rChild.val)
-      nodes_queue.append(rChild)
-    else:
-      treeList.append(None)
-  return treeList
+      subTrees = treesMap[i]
+      for i in range(len(subTrees)):
+        for j in range(len(subTrees)):
+          newRoot = TreeNode(0)
+          newRoot.left = cloneTree(subTrees[i])
+          newRoot.right = cloneTree(subTrees[j])
+          treesFound.append(newRoot)
+    i += 1
+    j -= 1
 
 
 def generateTreesRec(n, treesMap):
@@ -72,59 +105,20 @@ def generateTreesRec(n, treesMap):
     if n == 1:
       treesMap[1] = [TreeNode(0)]
     else:
-      # pdb.set_trace()
       generateTreesRec(n - 1, treesMap)
       currLevelTrees = []
-      for root in treesMap[n-1]:
-        newRoot1 = TreeNode(0)
-        newRoot1.left = TreeNode(root.val)
-        currLevelTrees.append(newRoot1)
-        newRoot2 = TreeNode(0)
-        newRoot2.right = TreeNode(root.val)
-        currLevelTrees.append(newRoot2)
-      i = 1
-      j = n - 2
-      while i > 0 and j > 0 and i <= j:
-        if i != j:
-          subTrees1 = treesMap[i]
-          subTrees2 = treesMap[j]
-          for root1 in subTrees1:
-            for root2 in subTrees2:
-              newRoot1 = TreeNode(0)
-              newRoot1.left = TreeNode(root1.val)
-              newRoot1.right = TreeNode(root2.val)
-              currLevelTrees.append(newRoot1)
-              newRoot2 = TreeNode(0)
-              newRoot2.left = TreeNode(root2.val)
-              newRoot2.right = TreeNode(root1.val)
-              currLevelTrees.append(newRoot2)
-        else:
-          subTrees = treesMap[i]
-          for i in range(len(subTrees)):
-            for j in range(len(subTrees)):
-              newRoot = TreeNode(0)
-              newRoot.left = TreeNode(subTrees[i].val)
-              newRoot.right = TreeNode(subTrees[j].val)
-              currLevelTrees.append(newRoot)
-        i += 1
-        j -= 1
+      gatherAllOneSidedTrees(n, currLevelTrees, treesMap)
+      gatherAllBothSidedTrees(n, currLevelTrees, treesMap)
       treesMap[n] = currLevelTrees
-
 
 
 def generateTrees(n):
   treesMap = {}
   generateTreesRec(n, treesMap)
-  # for n, trees in treesMap.items():
-  #   print(f'For {n} number of trees = {len(trees)}')
-  
   bsts = []
-  print(f'\nTrees at level {n}:')
   for tree in treesMap[n]:
     global nodeCounter
     nodeCounter = 1
     inorder_traversal(tree)
-    bsts.append(toList(tree))
-    print(f'  {bsts[-1]}')
-  
+    bsts.append(tree)
   return bsts
