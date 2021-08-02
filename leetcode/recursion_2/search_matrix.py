5e9f5571a05e0718e44940bcf2594ae11452894b
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-08-02 14:01:29 +0530

    LC-Recursion-2: Trying the more optimal 2 search technique for searching in a 2D matrix

--- leetcode/recursion_2/search_matrix.py
+++ leetcode/recursion_2/search_matrix.py
@@ -96,6 +96,62 @@ def searchMatrixRecursive(matrix, target, start, end):
   return False
 
 
+def searchBoundingCol(matrix, target, start, end):
+  si, sj = start
+  ei, ej = end
+  mj = int((sj + ej)/2)
+  minColVal = matrix[0][mj]
+  maxColVal = matrix[ei - 1][mj]
+  if target == minColVal:
+    return (True, )
+  elif target < minColVal:
+    return searchBoundingCol(matrix, target, start, (ei, mj))
+  elif target == maxColVal:
+    return (True, )
+  elif target > minColVal:
+    return searchBoundingCol(matrix, target, (si, mj), end)
+  else:
+    return (False, mj)
+
+
+def searchBoundingRows(matrix, target, col, iBounds):
+  pass
+
+
+def getTwoSearchBounds(start, end, rows, col):
+  pass
+
+
+def twoSearchRecursive(matrix, target, start, end):
+  si, sj = start
+  ei, ej = end
+  iSz = ei - si
+  jSz = ej - sj
+  assert iSz != 0
+  assert jSz != 0
+  if target < matrix[si][sj]:
+    return False
+  if target > matrix[ei][ej]:
+    return False
+  if iSz == 1 and jSz == 1:
+    return matrix[si][sj] == target
+  
+  found, col = searchBoundingCol(matrix, target, start, end)
+  if found:
+    return True
+  
+  found, rMin, rMax = searchBoundingRows(matrix, target, col, (si, ei))
+  if found:
+    return True
+
+  twoSearchBnds = getTwoSearchBounds(start, end, (rMin, rMax), col)
+  for bounds in twoSearchBnds:
+    newStart, newEnd = bounds
+    if twoSearchRecursive(matrix, target, newStart, newEnd):
+      return True
+  return False
+
+
 def searchMatrix(matrix, target):
   m = len(matrix)
   if m == 0:
