59e5f92e078d05906de609669c2316f0fee4d054
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-08-30 15:42:17 +0530

    LC-BinarySerach: Done finding peak

---
+++ leetcode/binary_search/find_peak.py
@@ -0,0 +1,59 @@
+#URL: https://leetcode.com/explore/learn/card/binary-search/126/template-ii/948/
+#Description
+"""
+A peak element is an element that is strictly greater than its neighbors.
+Given an integer array nums, find a peak element, and return its index. If the array contains 
+multiple peaks, return the index to any of the peaks.
+You may imagine that nums[-1] = nums[n] = -∞.
+You must write an algorithm that runs in O(log n) time.
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+
+
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index 
+number 5 where the peak element is 6.
+
+
+Constraints:
+
+1 <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+nums[i] != nums[i + 1] for all valid i.
+"""
+def findPeakElement(nums):
+  l = len(nums)
+  start = 0
+  end = l - 1
+  while start < end:
+    mid = (start + end)//2
+    if mid == 0:
+      break
+    elif mid == l - 1:
+      break
+    else:
+      if nums[mid] > nums[mid - 1]:
+        if nums[mid] > nums[mid + 1]:
+          return mid
+        else:
+          start = mid + 1
+      else:
+        end = mid
+  
+  if l == 1:
+    return 0
+  elif l > 1:
+    if nums[0] > nums[1]:
+      return 0
+    if nums[l - 1] > nums[l - 2]:
+      return l - 1
+
+  return -1
---
+++ leetcode/binary_search/tests/test_find_peak.py
@@ -0,0 +1,54 @@
+import pytest
+
+import binary_search.find_peak as prob
+
+class TestFindPeak:
+  def test_example1(self):
+    nums = [1,2,3,1]
+    res = 2
+    assert prob.findPeakElement(nums) == res
+  
+  def test_example2(self):
+    nums = [1,2,1,3,5,6,4]
+    res = 5
+    assert prob.findPeakElement(nums) == res
+  
+  def test_empty(self):
+    nums = []
+    res = -1
+    assert prob.findPeakElement(nums) == res
+  
+  def test_single(self):
+    nums = [1]
+    res = 0
+    assert prob.findPeakElement(nums) == res
+  
+  def test_two_ascending(self):
+    nums = [1, 2]
+    res = 1
+    assert prob.findPeakElement(nums) == res
+  
+  def test_two_descending(self):
+    nums = [2, 1]
+    res = 0
+    assert prob.findPeakElement(nums) == res
+  
+  def test_many_ascending(self):
+    nums = [1,2,3,4,5,6]
+    res = 5
+    assert prob.findPeakElement(nums) == res
+  
+  def test_many_descending(self):
+    nums = [5,4,3,2,1]
+    res = 0
+    assert prob.findPeakElement(nums) == res
+  
+  def test_descending_ascending(self):
+    nums = [5,4,3,2,1,2,3,4,5]
+    res = 0
+    assert prob.findPeakElement(nums) == res
+  
+  def test_lc1(self):
+    nums = [3,4,3,2,1]
+    res = 1
+    assert prob.findPeakElement(nums) == res
