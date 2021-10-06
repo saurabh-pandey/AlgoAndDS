94c96c79ac8ecb67b6eaf493152dd19242e4bbb4
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-10-06 13:39:20 +0530

    LC-NaryTree: Done recursive version of postorder traversal

--- leetcode/n_ary_tree/tests/test_postorder_traversal.py
+++ leetcode/n_ary_tree/tests/test_postorder_traversal.py
@@ -1,11 +1,11 @@
 import pytest
 
 import n_ary_tree.operations as n_tree
-import n_ary_tree.preorder_traversal as prob
+import n_ary_tree.postorder_traversal as prob
 
-class TestPreorderTraversal:
+class TestPostorderTraversal:
   def test_example1(self):
     root = [1,None,3,2,4,None,5,6]
-    res = [1,3,5,6,2,4]
+    res = [5,6,3,2,4,1]
     rootNode = n_tree.create(root)
-    assert prob.preorder(rootNode) == res
+    assert prob.postorder(rootNode) == res
