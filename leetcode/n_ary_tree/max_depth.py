48d8bea7df85885b7a4d80bb982340e557c7fab7
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-10-07 13:00:03 +0530

    LC-NaryTree: Done max-depth of n-ary tree using top-down approach

---
+++ leetcode/n_ary_tree/max_depth.py
@@ -0,0 +1,41 @@
+#URL: https://leetcode.com/explore/learn/card/n-ary-tree/131/recursion/919/
+#Description
+"""
+Given a n-ary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the 
+farthest leaf node.
+Nary-Tree input serialization is represented in their level order traversal, each group of children 
+is separated by the null value (See examples).
+
+
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: 3
+
+
+Example 2:
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: 5
+
+
+Constraints:
+
+The total number of nodes is in the range [0, 104].
+The depth of the n-ary tree is less than or equal to 1000.
+"""
+
+def maxDepthTopDown(node, depth, max_depth):
+  if node is None:
+    return max_depth
+  max_depth = depth if depth > max_depth else max_depth
+  for child in node.children:
+    if child is not None:
+      max_depth = maxDepthTopDown(child, depth + 1, max_depth)
+  return max_depth
+
+
+
+def maxDepth(root):
+  return maxDepthTopDown(root, 1, 0)
