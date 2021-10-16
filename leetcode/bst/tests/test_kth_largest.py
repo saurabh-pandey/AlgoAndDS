33ccfe4e08603aa33195421289f9ddb0771e2b91
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-10-16 10:07:30 +0530

    LC-BST: Added starting files for kth largest of a stream

---
+++ leetcode/bst/tests/test_kth_largest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from bst.kth_largest import KthLargest
+
+class TestKthLargest:
+    def test_example1(self):
+        kthLargest = KthLargest(3, [4, 5, 8, 2])
+        assert kthLargest.add(3) == 4
+        assert kthLargest.add(5) == 5
+        assert kthLargest.add(10) == 5
+        assert kthLargest.add(9) == 8
+        assert kthLargest.add(4) == 8
