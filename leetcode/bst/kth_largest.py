a8cd3f554e759c03b7f53fefd6870d4eef831981
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-10-19 14:08:57 +0530

    LC-BST: Trying to implement a counted BST tree for kth largest of a stream

--- leetcode/bst/kth_largest.py
+++ leetcode/bst/kth_largest.py
@@ -38,18 +38,72 @@ It is guaranteed that there will be at least k elements in the array when you se
 element.
 """
 import bisect
+# from bst.node import Node
+
+import pdb
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.count = 0
+    
+    def toStr(self):
+        return f"[val = {self.val}, count = {self.count}]"
+
 
 class KthLargest:
 
     def __init__(self, k, nums):
         self.k = k
         self.nums = nums
+        # pdb.set_trace()
+        self.root = None
+        for n in nums:
+            if self.root is None:
+                self.root = TreeNode(n)
+                self.root.count = 1
+            else:
+                self._insert(self.root, n)
         self.nums.sort()
-    
 
+
+    def _insert(self, node, val):
+        if val < node.val:
+            if node.left is None:
+                node.left = TreeNode(val)
+                node.left.count = 1
+            else:
+                self._insert(node.left, val)
+        else:
+            if node.right is None:
+                node.right = TreeNode(val)
+                node.right.count = 1
+                node.count += 1
+            else:
+                self._insert(node.right, val)
+                node.count += 1
+    
+    
     def add(self, val):
         it = bisect.bisect_left(self.nums, val)
         self.nums.insert(it, val)
         assert self.k <= len(self.nums)
         return self.nums[-self.k]
+    
+    
+    def new_add(self, val):
+        self._insert(self.root, val)
+        tree_list = []
+        nodes = [self.root]
+        while nodes:
+            node = nodes.pop(0)
+            tree_list.append(node.toStr())
+            if node.left:
+                nodes.append(node.left)
+            if node.right:
+                nodes.append(node.right)
+        print(tree_list)
+
         
--- leetcode/bst/tests/test_kth_largest.py
+++ leetcode/bst/tests/test_kth_largest.py
@@ -5,7 +5,9 @@ from bst.kth_largest import KthLargest
 class TestKthLargest:
     def test_example1(self):
         kthLargest = KthLargest(3, [4, 5, 8, 2])
+        kthLargest.new_add(3)
         assert kthLargest.add(3) == 4
+        kthLargest.new_add(5)
         assert kthLargest.add(5) == 5
         assert kthLargest.add(10) == 5
         assert kthLargest.add(9) == 8
