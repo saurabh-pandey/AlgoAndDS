5f418f3b4256518a90795ab173fd23ea6352a134
Saurabh Pandey <saurabhpandey85@gmail.com>
2021-12-15 11:40:30 +0530

    LC-HashTable: Done with hash map

--- leetcode/hash_table/tests/test_hash_map.py
+++ leetcode/hash_table/tests/test_hash_map.py
@@ -1,4 +1,5 @@
 import pytest
+import random
 
 from hash_table.hash_map import MyHashMap
 
@@ -13,3 +14,35 @@ class TestHashMap:
         assert myHashMap.get(2) == 1
         myHashMap.remove(2)
         myHashMap.get(2) == -1
+    
+    def test_my1(self):
+        myHashMap = MyHashMap()
+        mock_map = {}
+        max_key = 100000
+        max_val = 10
+        calls_sz = 10
+        for i in range(max_key + 1):
+            call_id = random.randint(0, calls_sz - 1)
+            key = random.randint(0, max_key)
+            if call_id < 5:
+                val = random.randint(0, max_val)
+                mock_map[key] = val
+                myHashMap.put(key, val)
+            elif call_id < 9:
+                if key in mock_map:
+                    assert myHashMap.get(key) == mock_map[key]
+                else:
+                    assert myHashMap.get(key) == -1
+            else:
+                mock_map.pop(key, None)
+                myHashMap.remove(key)
+        bucket_set = set()
+        for bucket in myHashMap.buckets:
+            for i in range(len(bucket)):
+                num = bucket[i][0]
+                if i > 0:
+                    assert num >= bucket[i - 1][0], f"Inversion found at {i - 1} and {i}"
+                assert num not in bucket_set, f"{num} seems to be duplicated"
+                bucket_set.add(num)
+        assert mock_map.keys() == bucket_set
+
