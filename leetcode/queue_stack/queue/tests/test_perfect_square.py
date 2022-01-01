972fca067b77b7dca1bd0d018b9a61c4bb2cf742
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-01-01 17:03:26 +0530

    LC-QueueStack: Cleaned up

--- leetcode/queue_stack/queue/tests/test_perfect_square.py
+++ leetcode/queue_stack/queue/tests/test_perfect_square.py
@@ -2,13 +2,9 @@ import pytest
 
 import queue_stack.queue.perfect_square as prob
 
-import pdb
 
 class TestPerfectSquare:
     def test_my_example1(self):
-        # print()
-        # pdb.set_trace()
-        # print(prob.numSquares(13))
         answers = {1:1, 2:2, 3:3, 4:1, 5:2, 6:3, 7:4, 8:2, 9:1, 10:2, 11:3, 12:3, 13:2, 14:3,
                    15:4, 16:1}
         for num, res in answers.items():
