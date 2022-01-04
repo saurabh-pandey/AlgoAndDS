3e5dc358945695be687e005fbe5790cd568f9d34
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-01-04 13:46:45 +0530

    GFG-DP: Added some more tests for sequence alignment problem

--- gfg/dp/tests/test_sequence_alignment.py
+++ gfg/dp/tests/test_sequence_alignment.py
@@ -32,3 +32,35 @@ class TestSequenceAlignment:
         res = ("AGGGCT", "A_GGCA", 5)
         prob.getMinPenaltyAlignment(X, Y, p_gap, p_xy)
         # assert prob.getMinPenaltyAlignment(X, Y, p_gap, p_xy) == res
+    
+    def test_my1(self):
+        X = "A"
+        Y = ""
+        p_gap = 3
+        p_xy = 2
+        res = ("A", "_", 3)
+        prob.getMinPenaltyAlignment(X, Y, p_gap, p_xy)
+    
+    def test_my2(self):
+        X = "A"
+        Y = "A"
+        p_gap = 3
+        p_xy = 2
+        res = ("A", "A", 0)
+        prob.getMinPenaltyAlignment(X, Y, p_gap, p_xy)
+    
+    def test_my3(self):
+        X = "A"
+        Y = "C"
+        p_gap = 3
+        p_xy = 2
+        res = ("A", "C", 2)
+        prob.getMinPenaltyAlignment(X, Y, p_gap, p_xy)
+    
+    def test_my4(self):
+        X = "A"
+        Y = "C"
+        p_gap = 2
+        p_xy = 5
+        res = ("A_", "_C", 4)
+        prob.getMinPenaltyAlignment(X, Y, p_gap, p_xy)
