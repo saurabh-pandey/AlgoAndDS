726faf08642eefca3a39142cd770a0f52fe45dd0
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-01-09 13:06:34 +0530

    LC-DP: Solved minimum cost climbing stairs

--- leetcode/dp/min_cost_climb.py
+++ leetcode/dp/min_cost_climb.py
@@ -38,9 +38,6 @@ Constraints:
 def minCostClimbingStairs(cost):
     sz = len(cost)
     A = [0 for _ in range(sz + 1)]
-    A[0] = 0
-    A[1] = cost[0]
     for i in range(2, sz + 1):
-        A[i] = min(A[i - 2] + cost[i - 1], A[i - 2] + cost[i - 2], A[i - 3] + cost[i - 2])
-    print(A)
+        A[i] = min(cost[i - 1] + A[i - 1], cost[i - 2] + A[i - 2])
     return A[-1]
