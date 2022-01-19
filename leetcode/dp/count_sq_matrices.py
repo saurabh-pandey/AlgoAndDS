df0110f024c06611edf443bf9e238c2ee2625ada
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-01-19 14:29:05 +0530

    LC-DP: All tests passing for counting all square submatrices

--- leetcode/dp/count_sq_matrices.py
+++ leetcode/dp/count_sq_matrices.py
@@ -41,8 +41,49 @@ Constraints:
 1 <= arr[0].length <= 300
 0 <= arr[i][j] <= 1
 """
+
+def isValidSquare(board, location, size):
+    i, j = location
+    if i + size > len(board):
+        return False
+    if j + size > len(board[0]):
+        return False
+    size_i = i + size
+    size_j = j + size
+    for i in range(i, size_i):
+        for j in range(j, size_j):
+            if board[i][j] != 1:
+                return False
+    return True
+
+
+def findSquares(size, seed, board):
+    squares = []
+    for i,j in seed:
+        if isValidSquare(board, (i, j), size):
+            squares.append((i, j))
+    return squares
+
+
 def countSquareSubmatrices(matrix):
     # Should we count all 1's. Store it in some dict. Next use neighbours to find bigger squares.
     # Every pass adds 1 row and 1 col for every grid point. We can check if all is 1. We might
     # shift by one. Do we just the previous iteration solutions for next?
-    pass
+    m = len(matrix)
+    if m == 0:
+        return 0
+    n = len(matrix[0])
+    if n == 0:
+        return 0
+    squares = []
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == 1:
+                squares.append((i, j))
+    num_squares = len(squares)
+    for size in range(2, min(m, n)):
+        squares = findSquares(size, squares, matrix)
+        if not squares:
+            break
+        num_squares += len(squares)
+    return num_squares
--- leetcode/dp/tests/test_count_sq_matrices.py
+++ leetcode/dp/tests/test_count_sq_matrices.py
@@ -9,3 +9,30 @@ class TestCountSqSubmatrices:
                   [0,1,1,1]]
         res = 15
         assert prob.countSquareSubmatrices(matrix) == res
+    
+    def test_example2(self):
+        matrix = [[1,0,1],
+                  [1,1,0],
+                  [1,1,0]]
+        res = 7
+        assert prob.countSquareSubmatrices(matrix) == res
+    
+    def test_empty1(self):
+        matrix = []
+        res = 0
+        assert prob.countSquareSubmatrices(matrix) == res
+    
+    def test_empty2(self):
+        matrix = [[]]
+        res = 0
+        assert prob.countSquareSubmatrices(matrix) == res
+    
+    def test_one(self):
+        matrix = [[1]]
+        res = 1
+        assert prob.countSquareSubmatrices(matrix) == res
+    
+    def test_zero(self):
+        matrix = [[0]]
+        res = 0
+        assert prob.countSquareSubmatrices(matrix) == res
