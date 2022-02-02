070996b17755a99ba3afdc20727908434571dab8
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-02-02 13:12:46 +0530

    LC-DP: Done with sorting number by power value

--- leetcode/dp/sort_by_power_val.py
+++ leetcode/dp/sort_by_power_val.py
@@ -43,9 +43,6 @@ Constraints:
 1 <= lo <= hi <= 1000
 1 <= k <= hi - lo + 1
 """
-from ast import Pow
-
-
 def powerVal(num):
     val = 0
     while num > 1:
@@ -56,12 +53,38 @@ def powerVal(num):
         val += 1
     return val
 
+
 def getKth_bf(lo, hi, k):
-    # print()
     power_vals = {}
     for i in range(lo, hi + 1):
         power_vals[i] = powerVal(i)
     sorted_power_vals = dict(sorted(power_vals.items(), key=lambda item: item[1]))
-    # print(sorted_power_vals)
+    nums = list(sorted_power_vals.keys())
+    return nums[k - 1]
+
+
+def fillPowerVal(num, power_vals):
+    if num not in power_vals:
+        if num % 2 == 0:
+            val = fillPowerVal(num // 2, power_vals)
+            power_vals[num] = val + 1
+        else:
+            val = fillPowerVal(3 * num + 1, power_vals)
+            power_vals[num] = val + 1
+    return power_vals[num]
+
+
+def calculatePowerVals(lo, hi):
+    power_vals = {1 : 0}
+    calc_power_vals = {}
+    for i in range(lo, hi + 1):
+        fillPowerVal(i, power_vals)
+        calc_power_vals[i] = power_vals[i]
+    return calc_power_vals
+
+
+def getKth(lo, hi, k):
+    calc_power_vals = calculatePowerVals(lo, hi)
+    sorted_power_vals = dict(sorted(calc_power_vals.items(), key=lambda item: item[1]))
     nums = list(sorted_power_vals.keys())
     return nums[k - 1]
--- leetcode/dp/tests/test_sort_by_power_val.py
+++ leetcode/dp/tests/test_sort_by_power_val.py
@@ -9,6 +9,7 @@ class TestSortByPowerValue:
         k = 2
         res = 13
         assert prob.getKth_bf(lo, hi, k) == res
+        assert prob.getKth(lo, hi, k) == res
     
     def test_example2(self):
         lo = 7
@@ -16,3 +17,16 @@ class TestSortByPowerValue:
         k = 4
         res = 7
         assert prob.getKth_bf(lo, hi, k) == res
+        assert prob.getKth(lo, hi, k) == res
+    
+    def test_my1(self):
+        lo = 2
+        hi = 30
+        k = 15
+        assert prob.getKth_bf(lo, hi, k) == prob.getKth(lo, hi, k)
+    
+    def test_my2(self):
+        lo = 20
+        hi = 500
+        k = 127
+        assert prob.getKth_bf(lo, hi, k) == prob.getKth(lo, hi, k)
