25a798d0d39a1cdd3fd183fe3363c77c28d56a6d
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-03-05 18:34:50 +0530

    My-DP: Starting with the optimal binary search tree problem

---
+++ myAttempts/dp/optimal_bst.py
@@ -0,0 +1,10 @@
+#Description
+"""
+Given some keys [1,2,...,n] and the keys' search probabilities = [p1, p2, ..., Pn]
+Now return the most optimal Binary Search Tree (BST) such that the average cost to query the nodes 
+in the tree is minimized.
+
+Assume that the query node is always in the tree. So no failed queries.
+
+NOTE: This was the third problem discussed in the DP part of Tim's algo-2 course
+"""
---
+++ myAttempts/dp/tests/test_optimal_bst.py
@@ -0,0 +1,8 @@
+import pytest
+
+import dp.optimal_bst as prob
+
+
+class TestOptimalBST:
+    def test_example1(self):
+        pass
