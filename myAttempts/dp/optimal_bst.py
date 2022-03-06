c2f3fc16dea57f016f77f2105effc788a6736f8c
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-03-06 15:57:38 +0530

    My-DP: Added some examples for optimal binary search tree problem

--- myAttempts/dp/optimal_bst.py
+++ myAttempts/dp/optimal_bst.py
@@ -6,5 +6,23 @@ in the tree is minimized.
 
 Assume that the query node is always in the tree. So no failed queries.
 
+Example 1
+For nodes 1,2,3,4 with probabilities = [2, 23, 73, 1]. The optimal BST is has root is 3 left child 
+is 2 and right is 4. Left child to 2 is 1
+
+
+Example 2
+Similar example, For nodes 1,2,3,4 with probabilities = [1, 34, 33, 32]. Optimal BST in this case 
+is same as above example.
+
+
+Example 3
+keys[] = {10, 12}, freq[] = {34, 50}. Frequency is the number of searches for each node. Optimal BST is root as 12.
+
+
+Example 4
+keys[] = {10, 12, 20}, freq[] = {34, 8, 50}. Optimal BST has 20 as root 10 as it left child. 12 is 
+the right child of 10.
+
 NOTE: This was the third problem discussed in the DP part of Tim's algo-2 course
 """
