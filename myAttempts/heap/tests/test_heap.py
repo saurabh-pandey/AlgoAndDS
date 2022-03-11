1982ab4d1ba60e1824f56371ce2aa0cf114df4a6
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-03-11 14:13:52 +0530

    My-Heap: Adding tests for heap

--- myAttempts/heap/tests/test_heap.py
+++ myAttempts/heap/tests/test_heap.py
@@ -4,7 +4,7 @@ import pytest
 from heap.heap import Heap
 
 class TestHeap:
-    def test_example1(self):
+    def test_insert(self):
         h = Heap()
         h.insert(5)
         assert h.extract_min() == 5
@@ -13,4 +13,34 @@ class TestHeap:
         h.insert(6)
         assert h.extract_min() == 3
         h.insert(4)
+        assert h.extract_min() == 3
+    
+    def test_delete(self):
+        h = Heap()
+        h.insert(5)
+        assert h.extract_min() == 5
+        h.insert(3)
+        assert h.extract_min() == 3
+        h.delete(3)
+        assert h.extract_min() == 5
+        h.insert(4)
+        assert h.extract_min() == 3
+        h.insert(2)
+        assert h.extract_min() == 2
+        h.delete(2)
+        assert h.extract_min() == 3
+    
+    def test_heapify(self):
+        h = Heap()
+        h.heapify([5,8,7,2,4,3])
+        assert h.extract_min() == 2
+        h.insert(1)
+        assert h.extract_min() == 1
+        h.delete(8)
+        assert h.extract_min() == 1
+        h.insert(9)
+        assert h.extract_min() == 1
+        h.delete(1)
+        assert h.extract_min() == 2
+        h.delete(2)
         assert h.extract_min() == 3
