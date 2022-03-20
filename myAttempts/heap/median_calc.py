bf69c4a36ad40484b9ea5b4701f9f05d1527b682
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-03-20 16:03:19 +0530

    My-Heap: Starting with median calculator for a running stream of numbers

---
+++ myAttempts/heap/median_calc.py
@@ -0,0 +1,18 @@
+#Description
+"""
+Given a stream of numbers compute the running median.
+
+NOTE: This problem was discussed in Tim's algo-1 course as an application of heap.
+"""
+
+
+
+class MedianCalculator:
+    def __init__(self) -> None:
+        pass
+
+    def add(self, num):
+        pass
+
+    def median(self):
+        pass
---
+++ myAttempts/heap/tests/test_median_calc.py
@@ -0,0 +1,17 @@
+import pytest
+
+import heap.median_calc as prob
+
+class TestMedianCalculator:
+    def test_example1(self):
+        median_calc = prob.MedianCalculator()
+        median_calc.add(1)
+        assert median_calc.median() == 1
+        median_calc.add(2)
+        assert median_calc.median() == 1.5
+        median_calc.add(3)
+        assert median_calc.median() == 2
+        median_calc.add(4)
+        assert median_calc.median() == 2.5
+        median_calc.add(5)
+        assert median_calc.median() == 3
