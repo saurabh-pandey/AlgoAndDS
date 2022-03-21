cf945d2b415721dca5812381792f60a7081f1477
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-03-21 18:05:57 +0530

    My-Heap: Still working on the running median problem

--- myAttempts/heap/median_calc.py
+++ myAttempts/heap/median_calc.py
@@ -13,10 +13,18 @@ class MedianCalculator:
         self.min_heap = Heap()
 
     def add(self, num):
+        # Assuming that max_heap is always the bigger one
+        # If max_heap is same as min_heap after insert max_heap will be bigger
+        # If max_heap is bigger then they have two option:
+        #   1. Either max_heap stays biggger
+        #   2. Or they become equal
+        # max_heap being smaller will be breaking the invariant
         max_heap_sz = self.max_heap.size()
         min_heap_sz = self.min_heap.size()
         # Invariant
         # min_heap_sz == max_heap_sz or (min_heap_sz - max_heap_sz) == 1
+        if max_heap_sz == 0:
+            self.max_heap.insert(num)
         max_val = self.max_heap.top()
         min_val = self.min_heap.top()
         if num <= max_val:
