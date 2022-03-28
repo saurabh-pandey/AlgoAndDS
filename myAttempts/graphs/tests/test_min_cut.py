50685edb0af8c8ee3f37a2a4dcfdd7fa5a59b737
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-03-28 14:31:10 +0530

    My-Graphs: Added some example graphs for testing min cut

--- myAttempts/graphs/tests/test_min_cut.py
+++ myAttempts/graphs/tests/test_min_cut.py
@@ -3,7 +3,42 @@ import pytest
 import graphs.min_cut as prob
 
 class TestMinimumCut:
-    def test_example1(self):
-        file_name = "squares.txt"
+    def test_single(self):
+        file_name = "single.txt"
+        res = 1
+        assert prob.find_min_cut(file_name) == res
+    
+    def test_linear(self):
+        file_name = "linear.txt"
+        res = 1
+        assert prob.find_min_cut(file_name) == res
+    
+    def test_triangle(self):
+        file_name = "triangle.txt"
+        res = 2
+        assert prob.find_min_cut(file_name) == res
+    
+    def test_square(self):
+        file_name = "square.txt"
+        res = 2
+        assert prob.find_min_cut(file_name) == res
+    
+    def test_square_all_diag(self):
+        file_name = "square_all_diag.txt"
+        res = 3
+        assert prob.find_min_cut(file_name) == res
+    
+    def test_square_diag(self):
+        file_name = "square_diag.txt"
+        res = 2
+        assert prob.find_min_cut(file_name) == res
+    
+    def test_two_triangles(self):
+        file_name = "two_triangles.txt"
+        res = 1
+        assert prob.find_min_cut(file_name) == res
+    
+    def test_two_squares(self):
+        file_name = "two_squares.txt"
         res = 2
         assert prob.find_min_cut(file_name) == res
