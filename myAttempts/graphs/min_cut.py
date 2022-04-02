8176b9d0f0f1024d479cb4b9fd34de9334b0ace6
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-04-02 16:25:51 +0530

    My-Graphs: Implemented edge collapse

--- myAttempts/graphs/min_cut.py
+++ myAttempts/graphs/min_cut.py
@@ -12,18 +12,40 @@ class Vertex:
     def __init__(self, value) -> None:
         assert value, "Value of vertex can't be empty"
         self.val = value
+        self.edges = []
     
     def get_value(self):
         return self.val
+    
+    def add_edge(self, edge):
+        self.edges.append(edge)
+    
+    def get_edges(self):
+        return self.edges
 
 class Edge:
-    def __init__(self, vert1, vert2) -> None:
-        assert vert1, "Vertex 1 of edge is None"
-        assert vert2, "Vertex 2 of edge is None"
-        self.verts = (vert1, vert2)
+    def __init__(self, v0, v1) -> None:
+        assert v0, "Vertex 0 of edge is None"
+        assert v1, "Vertex 1 of edge is None"
+        self.v0 = v0
+        self.v1 = v1
     
     def get_vertices(self):
-        return self.verts
+        return (self.v0, self.v1)
+    
+    def get_vertex0(self):
+        return self.v0
+    
+    def set_vertex0(self, v):
+        assert v, "Null cannot be set as vertex 0"
+        self.v0 = v
+    
+    def get_vertex1(self):
+        return self.v1
+
+    def set_vertex1(self, v):
+        assert v, "Null cannot be set as vertex 1"
+        self.v1 = v
 
 
 class Graph:
@@ -57,8 +79,12 @@ class Graph:
         self.vertices.append(new_vert)
         return new_vert
     
-    def remove_vertex(self, index):
-        del self.vertices[index]
+    def remove_vertex(self, value):
+        for i, v in enumerate(self.vertices):
+            if v.get_value() == value:
+                del self.vertices[i]
+                return
+        
     
     def add_edge(self, value1, value2):
         new_vert1 = Vertex(value1)
@@ -67,6 +93,19 @@ class Graph:
         self.vertices.append(new_vert2)
         new_edge = Edge(new_vert1, new_vert2)
         self.edges.append(new_edge)
+        new_vert1.add_edge(new_edge)
+        new_vert2.add_edge(new_edge)
+    
+    def remove_edge(self, edge):
+        for i, e in enumerate(self.edges):
+            if ((e.get_vertex0().get_value() == edge.get_vertex0().get_value())
+            and (e.get_vertex1().get_value() == edge.get_vertex1().get_value())):
+                del self.edges[i]
+                return
+            if ((e.get_vertex0().get_value() == edge.get_vertex1().get_value())
+            and (e.get_vertex1().get_value() == edge.get_vertex0().get_value())):
+                del self.edges[i]
+                return
     
     def num_vertices(self):
         return len(self.vertices)
@@ -83,7 +122,7 @@ class Graph:
 
     def print(self):
         for e in self.edges:
-            print(e.verts[0].val, " ", e.verts[1].val)
+            print(e.v0.val, " ", e.v1.val)
 
 
 def get_random_edge(g):
@@ -98,15 +137,24 @@ def collapse_edge(g, edge):
     merged_vert = g.add_vertex([vert1.get_value(), vert2.get_value()])
     
     # Update all edges with v1 or v2 as end point to point to merged vertex
-    # all_edges_with_vert1 as end-point
-    # for each with vert1 as end-point replace vert1 with merged vert
-    # for each with vert2 as end-point replace vert2 with merged vert
-    # all_edges_with_vert2 as end-point
+    for e in vert1.get_edges():
+        if e.get_vertex0().get_value() == vert1.get_value():
+            e.set_vertex0(merged_vert)
+        if e.get_vertex1().get_value() == vert1.get_value():
+            e.set_vertex1(merged_vert)
+    for e in vert2.get_edges():
+        if e.get_vertex0().get_value() == vert2.get_value():
+            e.set_vertex0(merged_vert)
+        if e.get_vertex1().get_value() == vert2.get_value():
+            e.set_vertex1(merged_vert)
     
     # Delete vert1 and vert2
+    g.remove_vertex(vert1)
+    g.remove_vertex(vert2)
     
     # Remove this edge
-    pass
+    g.remove_edge(edge)
+    
 
 def clear_self_loops(edge):
     # Post edge collapse now remove any self-loops
@@ -118,7 +166,7 @@ def find_min_cut(graphFile):
     g.print()
     # return
     num_verts = g.num_vertices()
-    if num_verts == 0:
+    if num_verts < 2:
         return 0
     num_iterations = num_verts * num_verts * math.ceil(math.log(num_verts))
     min_cut_sz = num_verts * (num_verts - 1)//2
