f7cc4b4869acdb1f01d76705a989932477979fd9
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-12-12 23:18:20 -0800

    LC-Arrays: Prepared remove elements

---
+++ leetcode/arrays/remove_element_a2.py
@@ -0,0 +1,4 @@
+from typing import List
+
+def remove_element(nums: List[int], val: int) -> int:
+    pass
--- leetcode/arrays/tests/test_remove_element.py
+++ leetcode/arrays/tests/test_remove_element.py
@@ -1,53 +1,63 @@
 import pytest
 
-import arrays.remove_element as prob
+import arrays.remove_element_a1 as a1
+import arrays.remove_element_a2 as a2
+
+# solutions = {"attempt1": a1.removeElement, "attempt2": a2.remove_element}
+solutions = {"attempt1": a1.removeElement}
 
 class TestRemoveElement:
   def test_example1(self):
-    nums = [3,2,2,3]
-    val = 3
-    newLen = prob.removeElement(nums, val)
-    assert newLen == 2
-    expected = [2,2]
-    assert expected == nums[:newLen]
+    for attempt, solve in solutions.items():
+      nums = [3,2,2,3]
+      val = 3
+      newLen = solve(nums, val)
+      assert newLen == 2, attempt
+      expected = [2,2]
+      assert expected == nums[:newLen], attempt
   
   def test_example2(self):
-    nums = [0,1,2,2,3,0,4,2]
-    val = 2
-    newLen = prob.removeElement(nums, val)
-    assert newLen == 5
-    expected = [0,1,4,0,3]
-    assert expected == nums[:newLen]
+    for attempt, solve in solutions.items():
+      nums = [0,1,2,2,3,0,4,2]
+      val = 2
+      newLen = solve(nums, val)
+      assert newLen == 5, attempt
+      expected = [0,1,4,0,3]
+      assert expected == nums[:newLen], attempt
   
   def test_empty(self):
-    nums = []
-    val = 2
-    newLen = prob.removeElement(nums, val)
-    assert newLen == 0
-    expected = []
-    assert expected == nums
+    for attempt, solve in solutions.items():
+      nums = []
+      val = 2
+      newLen = solve(nums, val)
+      assert newLen == 0, attempt
+      expected = []
+      assert expected == nums, attempt
   
   def test_remove_all(self):
-    nums = [1,1,1]
-    val = 1
-    newLen = prob.removeElement(nums, val)
-    assert newLen == 0
-    expected = []
-    assert expected == nums[:newLen]
+    for attempt, solve in solutions.items():
+      nums = [1,1,1]
+      val = 1
+      newLen = solve(nums, val)
+      assert newLen == 0, attempt
+      expected = []
+      assert expected == nums[:newLen], attempt
   
   def test_remove_none(self):
-    nums = [2,2,2]
-    val = 1
-    newLen = prob.removeElement(nums, val)
-    assert newLen == 3
-    expected = [2,2,2]
-    assert expected == nums[:newLen]
+    for attempt, solve in solutions.items():
+      nums = [2,2,2]
+      val = 1
+      newLen = solve(nums, val)
+      assert newLen == 3, attempt
+      expected = [2,2,2]
+      assert expected == nums[:newLen], attempt
   
   def test_another_example(self):
-    nums = [0,4,4,0,4,4,4,0,2]
-    val = 4
-    newLen = prob.removeElement(nums, val)
-    assert newLen == 4
-    expected = [0,2,0,0]
-    assert expected == nums[:newLen]
+    for attempt, solve in solutions.items():
+      nums = [0,4,4,0,4,4,4,0,2]
+      val = 4
+      newLen = solve(nums, val)
+      assert newLen == 4, attempt
+      expected = [0,2,0,0]
+      assert expected == nums[:newLen], attempt
   
