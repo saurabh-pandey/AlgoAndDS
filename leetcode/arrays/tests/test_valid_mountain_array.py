556babcb91f8c2ff70115d90df168507aac8ec56
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-12-16 22:52:06 -0800

    LC-Arrays: Preparing to solve valid mountain array

--- leetcode/arrays/tests/test_valid_mountain_array.py
+++ leetcode/arrays/tests/test_valid_mountain_array.py
@@ -1,37 +1,52 @@
 import pytest
 
-import arrays.valid_mountain_array as prob
+import arrays.valid_mountain_array_a1 as a1
+import arrays.valid_mountain_array_a2 as a2
+
+solutions = {"attempt1": a1.validMountainArray,}
+            #  "attempt2": a2.valid_mountain_array}
 
 class TestValidMountainArray:
   def test_example1(self):
-    assert prob.validMountainArray([2,1]) == False
+    for attempt, solve in solutions.items():
+      assert solve([2,1]) == False, attempt
   
   def test_example2(self):
-    assert prob.validMountainArray([3,5,5]) == False
+    for attempt, solve in solutions.items():
+      assert solve([3,5,5]) == False, attempt
   
   def test_example3(self):
-    assert prob.validMountainArray([0,3,2,1]) == True
+    for attempt, solve in solutions.items():
+      assert solve([0,3,2,1]) == True, attempt
   
   def test_empty(self):
-    assert prob.validMountainArray([]) == False
+    for attempt, solve in solutions.items():
+      assert solve([]) == False, attempt
   
   def test_len_1(self):
-    assert prob.validMountainArray([2]) == False
+    for attempt, solve in solutions.items():
+      assert solve([2]) == False, attempt
   
   def test_all_equal(self):
-    assert prob.validMountainArray([1,1,1,1]) == False
+    for attempt, solve in solutions.items():
+      assert solve([1,1,1,1]) == False, attempt
   
   def test_some_equal(self):
-    assert prob.validMountainArray([0,1,2,2]) == False
+    for attempt, solve in solutions.items():
+      assert solve([0,1,2,2]) == False, attempt
   
   def test_oscillation(self):
-    assert prob.validMountainArray([1,4,2,7,3]) == False
+    for attempt, solve in solutions.items():
+      assert solve([1,4,2,7,3]) == False, attempt
   
   def test_ascending_only(self):
-    assert prob.validMountainArray([1,2,3,4,5]) == False
+    for attempt, solve in solutions.items():
+      assert solve([1,2,3,4,5]) == False, attempt
   
   def test_descending_only(self):
-    assert prob.validMountainArray([5,4,3,2,1,0]) == False
+    for attempt, solve in solutions.items():
+      assert solve([5,4,3,2,1,0]) == False, attempt
 
   def test_another_valid(self):
-    assert prob.validMountainArray([0,1,2,3,4,5,4,3,2,1,0]) == True
+    for attempt, solve in solutions.items():
+      assert solve([0,1,2,3,4,5,4,3,2,1,0]) == True, attempt
---
+++ leetcode/arrays/valid_mountain_array_a2.py
@@ -0,0 +1,4 @@
+from typing import List
+
+def valid_mountain_array(arr: List[int]) -> bool:
+    pass
