1813f02bc10775f520b1307758ab3a7fa78da0a2
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-12-21 00:12:48 -0800

    LC-Arrays: Preparing for height checker problem

---
+++ leetcode/arrays/height_checker_a2.py
@@ -0,0 +1,4 @@
+from typing import List
+
+def height_checker(arr: List[int]) -> None:
+    pass
--- leetcode/arrays/tests/test_height_checker.py
+++ leetcode/arrays/tests/test_height_checker.py
@@ -1,29 +1,38 @@
 import pytest
 
-import arrays.height_checker as prob
+import arrays.height_checker_a1 as a1
+import arrays.height_checker_a2 as a2
+
+solutions = {"attempt1": a1.heightChecker,}
+            #  "attempt2": a2.height_checker}
 
 class TestHeightChecker:
   def test_example1(self):
-    heights = [1,1,4,2,1,3]
-    numSelected = prob.heightChecker(heights)
-    assert numSelected == 3
+    for attempt, solve in solutions.items():
+      heights = [1,1,4,2,1,3]
+      numSelected = solve(heights)
+      assert numSelected == 3, attempt
   
   def test_example2(self):
-    heights = [5,1,2,3,4]
-    numSelected = prob.heightChecker(heights)
-    assert numSelected == 5
+    for attempt, solve in solutions.items():
+      heights = [5,1,2,3,4]
+      numSelected = solve(heights)
+      assert numSelected == 5, attempt
   
   def test_example3(self):
-    heights = [1,2,3,4,5]
-    numSelected = prob.heightChecker(heights)
-    assert numSelected == 0
+    for attempt, solve in solutions.items():
+      heights = [1,2,3,4,5]
+      numSelected = solve(heights)
+      assert numSelected == 0, attempt
   
   def test_empty(self):
-    heights = []
-    numSelected = prob.heightChecker(heights)
-    assert numSelected == 0
+    for attempt, solve in solutions.items():
+      heights = []
+      numSelected = solve(heights)
+      assert numSelected == 0, attempt
   
   def test_reverse(self):
-    heights = [5,4,3,2,1]
-    numSelected = prob.heightChecker(heights)
-    assert numSelected == 4
+    for attempt, solve in solutions.items():
+      heights = [5,4,3,2,1]
+      numSelected = solve(heights)
+      assert numSelected == 4, attempt
