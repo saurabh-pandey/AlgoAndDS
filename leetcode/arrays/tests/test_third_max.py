0f20a63ac5b0278ecb57a96f408a1c6f890bbd2a
Saurabh Pandey <saurabhpandey85@gmail.com>
2022-12-21 23:53:55 -0800

    LC-Arrays: Preparing for third max

--- leetcode/arrays/tests/test_third_max.py
+++ leetcode/arrays/tests/test_third_max.py
@@ -1,16 +1,24 @@
 import pytest
 
-import arrays.third_max as prob
+import arrays.third_max_a1 as a1
+import arrays.third_max_a2 as a2
+
+solutions = {"attempt1": a1.thirdMax,}
+            #  "attempt2": a2.third_max}
 
 class TestThirdMax:
   def test_example1(self):
-    assert prob.thirdMax([3,2,1]) == 1
+    for attempt, solve in solutions.items():
+      assert solve([3,2,1]) == 1, attempt
   
   def test_example2(self):
-    assert prob.thirdMax([1,2]) == 2
+    for attempt, solve in solutions.items():
+      assert solve([1,2]) == 2, attempt
   
   def test_example3(self):
-    assert prob.thirdMax([2,2,3,1]) == 1
+    for attempt, solve in solutions.items():
+      assert solve([2,2,3,1]) == 1, attempt
   
   def test_min_thirdmax(self):
-    assert prob.thirdMax([1,2,-2147483648]) == -2147483648
+    for attempt, solve in solutions.items():
+      assert solve([1,2,-2147483648]) == -2147483648, attempt
---
+++ leetcode/arrays/third_max_a2.py
@@ -0,0 +1,4 @@
+from typing import List
+
+def third_max(arr: List[int]) -> int:
+    pass
