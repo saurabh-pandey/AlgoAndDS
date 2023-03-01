136b23b6e6812b17cf4dda8bb23e2aa444f36397
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-01 17:52:58 +0530

    EPI-Python: Done with computing mod of only power 2 numbers

--- epi/ch01_primitives/parity_mod_pow_2_a1.py
+++ epi/ch01_primitives/parity_mod_pow_2_a1.py
@@ -2,4 +2,7 @@
 Compute x mod a power of 2, e.g., returns 13 for 77 mod 64
 '''
 def mod_pow_2(x: int, power: int) -> int:
-    pass
+    den = 1 << power
+    quo = x >> power
+    mod = x - quo * den
+    return mod
--- epi/ch01_primitives/tests/test_parity_mod_pow_2.py
+++ epi/ch01_primitives/tests/test_parity_mod_pow_2.py
@@ -1,6 +1,6 @@
 import pytest
 
-import parity_mod_pow_2_a1 as a1
+import ch01_primitives.parity_mod_pow_2_a1 as a1
 
 solutions = {"attempt1": a1.mod_pow_2}
 
@@ -11,4 +11,28 @@ class TestModPower2:
             power = 6 # 2^6 = 64
             res = solve(x, power)
             expected = 13
+            assert res == expected, attempt
+    
+    def test_example2(self):
+        for attempt, solve in solutions.items():
+            x = 64
+            power = 6 # 2^6 = 64
+            res = solve(x, power)
+            expected = 0
+            assert res == expected, attempt
+    
+    def test_example3(self):
+        for attempt, solve in solutions.items():
+            x = 3
+            power = 1 # 2^1 = 2
+            res = solve(x, power)
+            expected = 1
+            assert res == expected, attempt
+    
+    def test_example4(self):
+        for attempt, solve in solutions.items():
+            x = 77
+            power = 2 # 2^2 = 4
+            res = solve(x, power)
+            expected = 1
             assert res == expected, attempt
