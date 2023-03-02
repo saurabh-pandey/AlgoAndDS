29049089fb1670b36b4613ada36caf4469aba77b
Saurabh Pandey <saurabhpandey85@gmail.com>
2023-03-02 11:48:46 +0530

    EPI-Python: Done with right propagating rightmost set bit

--- epi/ch01_primitives/parity_right_propagate_a1.py
+++ epi/ch01_primitives/parity_right_propagate_a1.py
@@ -3,4 +3,8 @@ Right propagate the rightmost set bit in x, e.g., turns (01010000) to (01011111)
 '''
 
 def right_propagate(x: int) -> int:
-    pass
+    last_set_bit = x & ~(x - 1)
+    while last_set_bit:
+        x |= last_set_bit
+        last_set_bit >>= 1
+    return x
--- epi/ch01_primitives/tests/test_parity_right_propagate.py
+++ epi/ch01_primitives/tests/test_parity_right_propagate.py
@@ -1,6 +1,6 @@
 import pytest
 
-import parity_right_propagate_a1 as a1
+import ch01_primitives.parity_right_propagate_a1 as a1
 
 solutions = {"attempt1": a1.right_propagate}
 
@@ -10,4 +10,25 @@ class TestRightPropagate:
             x = 0b01010000
             res = solve(x)
             expected = 0b01011111
+            assert res == expected, attempt
+    
+    def test_example2(self):
+        for attempt, solve in solutions.items():
+            x = 0b1
+            res = solve(x)
+            expected = 0b1
+            assert res == expected, attempt
+    
+    def test_example3(self):
+        for attempt, solve in solutions.items():
+            x = 0b0
+            res = solve(x)
+            expected = 0b0
+            assert res == expected, attempt
+    
+    def test_example4(self):
+        for attempt, solve in solutions.items():
+            x = 0b1000
+            res = solve(x)
+            expected = 0b1111
             assert res == expected, attempt
